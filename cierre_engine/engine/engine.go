d3390d7f3171aabed0425cf6f34e916778e7432d
Jordy Montalvo <jordyjosephmontalvo@gmail.com>
2026-06-22 10:57:23 -0500

    fix(cierre): apply rank ReconsumoRequired rules accurately using ActivatedInternal

--- cierre_engine/engine/engine.go
+++ cierre_engine/engine/engine.go
@@ -113,7 +113,7 @@ func (e *CierreEngine) CalculateRank(id string) string {
 		}
 
 		// 1. Check Reconsumo
-		if !user.Activated && reconsumo < r.ReconsumoRequired {
+		if !user.ActivatedInternal && reconsumo < r.ReconsumoRequired {
 			continue
 		}
 
